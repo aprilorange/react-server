bfd36b8466db7daeebd86de47afcf23e08c66b1b
aprilorange <aprilorange.net@icloud.com>
2015-08-29 19:15:53 +0800

    rebuild

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp'),
   babel = require('gulp-babel'),
   webpack = require('webpack-stream'),
-  webpackConfig = require('./webpack.config')
-
+  webpackConfig = require('./webpack.config'),
+  nib = require('nib'),
+  stylus = require('gulp-stylus')
+ 
 var paths = {
-  babel: './src/*/*.js'
+  babel: './src/*/*.js',
+  stylMain: './src/styl/main.styl',
+  css: './build/css'
 }
-
+ 
 gulp.task('babel', function() {
   return gulp.src(paths.babel)
     .pipe(babel({
@@ -14,19 +18,34 @@ gulp.task('babel', function() {
       }))
     .pipe(gulp.dest('./core'))
 })
-
+ 
 gulp.task('webpack-dev', function() {
   return gulp.src('./src/app/index.js')
     .pipe(webpack(webpackConfig))
     .pipe(gulp.dest('./build/js'))
 })
-
+ 
+gulp.task('webpack', function() {
+  return gulp.src('./src/app/index.js')
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest('./build/js'))
+})
+ 
+gulp.task('stylus', function() {
+  return gulp.src(paths.stylMain)
+    .pipe(stylus({
+      use: [nib()],
+      import: ['nib']
+    }))
+    .pipe(gulp.dest(paths.css))
+})
+ 
 gulp.task('watch', function() {
   gulp.watch(paths.babel, ['babel'])
 })
-
-gulp.task('build', ['babel'])
-
-gulp.task('build-dev', ['babel', 'webpack-dev'])
-
+ 
+gulp.task('build', ['babel', 'webpack', 'stylus'])
+ 
+gulp.task('build-dev', ['babel', 'webpack-dev', 'stylus'])
+ 
 gulp.task('default', ['build-dev', 'watch'])
